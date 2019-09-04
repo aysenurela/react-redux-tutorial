5738fcfe6a0ae87c25742713dbcda7f952ecd11b
aysenur.ela <aysenur.ela@solvoyo.com>
2019-09-04 18:58:47 +0300

    redux intro

--- src/index.js
+++ src/index.js
@@ -1,12 +1,106 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { combineReducers, createStore } from 'redux'
+
+const state = {
+    todos: [{ text: "eating", completed: true }, { text: "exercise", completed: false }],
+    visibilityFilter: "SHOW_ALL"
+}
+
+const initialState = {
+    visibilityFilter: 'SHOW_ALL',
+    todos: []
+}
+
+
+//An action is a plain JavaScript object that describes what happened.
+
+const actionAdd = { type: 'ADD_TODO', text: 'Go to swimming pool' }
+const actionToggle = { type: 'TOGGLE_TODO', index: 1 }
+const actionVisibilityFilter = { type: 'SET_VISIBILITY_FILTER', filter: 'SHOW_COMPLETED' }
+
+//In Redux, action creators simply return an action:
+function addTodo(text) {
+    return { type: "ADD_TODO", text }
+}
+function toggleTodo(index) {
+    return { type: "TOGGLE_TODO", index }
+}
+function setVisibilityFilter(filter) {
+    return { type: "SET_VISIBILITY_FILTER", filter }
+}
+
+
+//REDUCER////////////////////////////////////////////////////////////////////////////////////////
+//Finally, to tie state and actions together, we write a function called a reducer.
+//it’s just a function that takes state and action as arguments, and returns the next state of the app.
+
+function todoApp(state = initialState, action) {
+    switch (action.type) {
+        case "SET_VISIBILITY_FILTER":
+            return Object.assign({}, state, {
+                visibilityFilter: action.filter
+            })
+        case "ADD_TODO":
+            return Object.assign({}, state, {
+                todos: [
+                    ...state.todos,
+                    {
+                        text: action.text,
+                        completed: false
+                    }
+                ]
+            })
+        case "TOGGLE_TODO":
+            return Object.assign({}, state, {
+                todos: state.todos.map((todo, index) => {
+                    if (index === action.index) {
+                        return Object.assign({}, todo, {
+                            completed: !todo.completed
+                        })
+                    }
+                    return todo
+                })
+            })
+        default:
+            return state
+    }
+}
+
+//REDUCER////////////////////////////////////////////////////////////////////////////////////////
+
+
+const store = createStore(todoApp)
+console.log("store", store)
+console.log("state", store.getState())
+
+
+// Dispatch some actions
+store.dispatch(addTodo('Learn about actions'))
+console.log("state", store.getState())
+store.dispatch(addTodo('Learn about reducers'))
+console.log("state", store.getState())
+store.dispatch(addTodo('Learn about store'))
+console.log("state", store.getState())
+
+
+
+
+
+
+function App() {
+    return (
+        <div className="App">
+            <input></input>
+            <button>Add</button>
+            <ul>
+                {state.todos.map(function (elem) {
+                    return <li> {elem.text} </li>
+                })}
+            </ul>
+        </div>
+    );
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
